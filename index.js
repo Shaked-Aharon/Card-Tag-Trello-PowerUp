e7ce7b01bb8950e0893e4e60d918e3889e0758c2
Shaked Aharon <shakedaha@matrix.co.il>
2023-05-11 00:58:12 +0300

    little refactor

--- index.js
+++ index.js
@@ -4,82 +4,83 @@ window.TrelloPowerUp.initialize({
     return t
       .card("name")
       .get("name")
-      .then(function (cardName) {
-        return [
-          {
-            // It's best to use static badges unless you need your
-            // badges to refresh.
-            // You can mix and match between static and dynamic
-            text: "Static",
-            color: 'purple',
-          },
-        ];
-      });
+      .then(showBadge);
   },
-    'card-buttons': function(t, options) {
-      return [{
-        icon: {
-          dark: 'https://example.com/icon.svg',
-          light: 'https://example.com/icon.svg'
-        },
-        text: 'Set Tag',
-        callback: tagSelectionPopup
-      }];
-    },
-    'card-detail-badges': function(t, options) {
-      return t.get('card', 'shared', 'tag')
-        .then(function(tag) {
-          if (!tag) {
-            return [];
-          }
-          return [{
-            text: tag.text,
-            color: tag.color,
-            showOnClose: true
-          }];
-        });
-    }
+  'card-buttons': function (t, options) {
+    return [{
+      icon: {
+        dark: 'https://example.com/icon.svg',
+        light: 'https://example.com/icon.svg'
+      },
+      text: 'Set Tag',
+      callback: tagSelectionPopup
+    }];
+  },
+  'card-detail-badges': function (t, options) {
+    return t.get('card', 'shared', 'tag')
+      .then(showDetailsBadge);
+  }
+});
+
+function getTagColor(value) {
+  switch (value) {
+    case 'bug':
+      return 'red';
+    case 'task':
+      return 'yellow';
+    case 'story':
+      return 'blue';
+    case 'epic':
+      return 'green';
+    default:
+      return 'gray';
+  }
+}
+
+
+function tagSelectionPopup(t) {
+  return t.popup({
+    title: 'Set Tag',
+    items: [
+      { text: 'Bug', callback: handleTagSelection.bind({ text: 'Bug', value: 'bug' }) },
+      { text: 'Task', callback: handleTagSelection.bind({ text: 'Task', value: 'task' }) },
+      { text: 'Story', callback: handleTagSelection.bind({ text: 'Story', value: 'story' }) },
+      { text: 'Epic', callback: handleTagSelection.bind({ text: 'Epic', value: 'epic' }) }
+    ],
   });
-  
-  function getTagColor(value) {
-    switch (value) {
-      case 'bug':
-        return 'red';
-      case 'task':
-        return 'yellow';
-      case 'story':
-        return 'blue';
-      case 'epic': 
-        return 'green';
-      default:
-        return 'gray';
-    }
-  }
-  
+}
 
-  function tagSelectionPopup(t) {
-    return t.popup({
-      title: 'Set Tag',
-      items: [
-        { text: 'Bug', callback: handleTagSelection.bind({text: 'Bug', value: 'bug'}) },
-        { text: 'Task', callback: handleTagSelection.bind({text: 'Task', value: 'task'}) },
-        { text: 'Story', callback: handleTagSelection.bind({text: 'Story', value: 'story'}) },
-        { text: 'Epic', callback: handleTagSelection.bind({text: 'Epic', value: 'epic'}) }
-      ],
+function handleTagSelection(t, selection) {
+  if (!selection) {
+    alert('no selection')
+    return;
+  }
+  var tag = {
+    text: this.text,
+    color: getTagColor(this.value)
+  };
+  return t.set('card', 'shared', 'tag', tag)
+    .then(function () {
+      return t.closePopup();
     });
-  }
+}
 
-  function handleTagSelection(t, selection) {
-    if (!selection) {
-      alert('no selection')
-      return;
-    }
-    var tag = {
-      text: this.text,
-      color: getTagColor(this.value)
-    };
-    return t.set('card', 'shared', 'tag', tag)
-      .then(function() {
-        return t.closePopup();
-      });
+function showDetailsBadge(tag) {
+  if (!tag) {
+    return [];
   }
+  return [{
+    text: tag.text,
+    color: tag.color,
+    showOnClose: true
+  }];
+}
+
+function showBadge (tag) {
+  return [
+    {
+      text: tag.text,
+      color: tag.color,
+    },
+  ];
+}
