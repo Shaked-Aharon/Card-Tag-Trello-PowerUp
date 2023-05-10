6eb7c59e8e33001c005a7ef769b3d55141b2e64d
Shaked Aharon <shakedaha@matrix.co.il>
2023-05-10 23:33:41 +0300

    test

--- index.js
+++ index.js
@@ -1,25 +1,51 @@
 var Promise = TrelloPowerUp.Promise;
 
-TrelloPowerUp.initialize({
-  'card-buttons': function (t, options) {
+var GRAY_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg';
+
+var onBtnClick = function (t, opts) {
+  console.log('Someone clicked the button');
+};
+
+Promise.initialize({
+  'card-buttons': function (t, opts) {
     return [{
-      icon: './icon.svg',
-      text: 'Custom Field',
-      callback: function (t) {
-        return t.popup({
-          title: 'My Custom Field',
-          url: './custom-field.html',
-          height: 184,
-          callback: function () {
-            return {
-              customField: {
-                title: 'My Custom Field',
-                value: t.card('custom', 'customField')
-              }
-            };
-          }
-        });
-      }
+      // usually you will provide a callback function to be run on button click
+      // we recommend that you use a popup on click generally
+      icon: GRAY_ICON, // don't use a colored icon here
+      text: 'Open Popup',
+      callback: onBtnClick,
+      condition: 'edit'
+    }, {
+      // but of course, you could also just kick off to a url if that's your thing
+      icon: GRAY_ICON,
+      text: 'Just a URL',
+      condition: 'always',
+      url: 'https://developer.atlassian.com/cloud/trello',
+      target: 'Trello Developer Site' // optional target for above url
     }];
   }
 });
+
+// TrelloPowerUp.initialize({
+//   'card-buttons': function (t, options) {
+//     return [{
+//       icon: './icon.svg',
+//       text: 'Custom Field',
+//       callback: function (t) {
+//         return t.popup({
+//           title: 'My Custom Field',
+//           url: './custom-field.html',
+//           height: 184,
+//           callback: function () {
+//             return {
+//               customField: {
+//                 title: 'My Custom Field',
+//                 value: t.card('custom', 'customField')
+//               }
+//             };
+//           }
+//         });
+//       }
+//     }];
+//   }
+// });
