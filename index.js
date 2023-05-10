804014cd1454520b068672297561952b1549df6b
Shaked Aharon <shakedaha@matrix.co.il>
2023-05-11 00:47:15 +0300

    Test Another Card Badge

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 window.TrelloPowerUp.initialize({
-  'card-badges': function(t, opts) {
-    return t.get('card', 'shared', 'tag')
-      .then(function(tag) {
-        if (!tag) {
-          return [];
-        }
-        return [{
-          text: tag.text,
-          color: tag.color
-        }];
+  "card-badges": function (t, opts) {
+    let cardAttachments = opts.attachments; // Trello passes you the attachments on the card
+    return t
+      .card("name")
+      .get("name")
+      .then(function (cardName) {
+        return [
+          {
+            // Dynamic badges can have their function rerun
+            // after a set number of seconds defined by refresh.
+            // Minimum of 10 seconds.
+            dynamic: function () {
+              // we could also return a Promise that resolves to
+              // this as well if we needed to do something async first
+              return {
+                text: "Dynamic " + (Math.random() * 100).toFixed(0).toString(),
+                icon: "./images/icon.svg",
+                color: "green",
+                refresh: 10, // in seconds
+              };
+            },
+          },
+          {
+            // It's best to use static badges unless you need your
+            // badges to refresh.
+            // You can mix and match between static and dynamic
+            text: "Static",
+            icon: HYPERDEV_ICON, // for card front badges only
+            color: null,
+          },
+        ];
       });
   },
     'card-buttons': function(t, options) {
