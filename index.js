293dfe765ffb86b5b54850528704c2a9ad5b3c09
Shaked Aharon <shakedaha@matrix.co.il>
2023-05-11 00:01:47 +0300

    CHAT-GPT

--- index.js
+++ index.js
@@ -1,128 +1,63 @@
 window.TrelloPowerUp.initialize({
-  "card-detail-badges": function (t, opts) {
-    return t
-      .card("name")
-      .get("name")
-      .then(function (cardName) {
-        console.log("We just loaded the card name for fun: " + cardName);
-
-        return [
-          {
-            // dynamic badges can have their function rerun after a set number
-            // of seconds defined by refresh. Minimum of 10 seconds.
-            dynamic: function () {
-              // we could also return a Promise that resolves to this
-              // as well if we needed to do something async first
-              return {
-                title: "Detail Badge",
-                text: "Dynamic " + (Math.random() * 100).toFixed(0).toString(),
-                color: randomBadgeColor(),
-                refresh: 10, // in seconds
+    'card-buttons': function(t, options) {
+      return [{
+        icon: {
+          dark: 'https://example.com/icon.svg',
+          light: 'https://example.com/icon.svg'
+        },
+        text: 'Set Tag',
+        callback: function(t) {
+          return t.popup({
+            title: 'Set Tag',
+            items: [
+              { text: 'Bug', value: 'bug' },
+              { text: 'Task', value: 'task' },
+              { text: 'Story', value: 'story' },
+              { text: 'Epic', value: 'epic' }
+            ],
+            callback: function(t, selection) {
+              if (!selection) {
+                return;
+              }
+              var tag = {
+                text: selection.text,
+                color: getTagColor(selection.value)
               };
-            },
-          },
-          {
-            // its best to use static badges unless you need your badges
-            // to refresh you can mix and match between static and dynamic
-            title: "Detail Badge",
-            text: "Static",
-            color: null,
-          },
-          {
-            // card detail badges (those that appear on the back of cards)
-            // also support callback functions so that you can open for example
-            // open a popup on click
-            title: "Popup Detail Badge",
-            text: "Popup",
-            callback: function (t, opts) {
-              // function to run on click
-              // do something
-            },
-          },
-          {
-            // or for simpler use cases you can also provide a url
-            // when the user clicks on the card detail badge they will
-            // go to a new tab at that url
-            title: "URL Detail Badge",
-            text: "URL",
-            url: "https://trello.com/home",
-            target: "Trello Landing Page", // optional target for above url
-          },
-        ];
-      });
-  },
-});
-
-
-// var GRAY_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg';
-
-// var onBtnClick = function (t, opts) {
-//   console.log('Someone clicked the button');
-// };
-// TrelloPowerUp.initialize({
-//   'card-buttons': function (t, opts) {
-//     return [
-//       {
-//       // usually you will provide a callback function to be run on button click
-//       // we recommend that you use a popup on click generally
-//       icon: GRAY_ICON, // don't use a colored icon here
-//       text: 'Open Popup',
-//       callback: onBtnClick,
-//       condition: 'edit'
-//     },
-//     {
-//       // but of course, you could also just kick off to a url if that's your thing
-//       icon: GRAY_ICON,
-//       text: 'Just a URL',
-//       condition: 'always',
-//       url: 'https://developer.atlassian.com/cloud/trello',
-//       target: 'Trello Developer Site' // optional target for above url
-//     },
-//     {
-//       icon: GRAY_ICON,
-//       text: 'Test Field',
-//       callback: function (t) {
-//         // return t.popup({
-//         //   title: 'My Custom Field',
-//         //   url: './custom-field.html',
-//         //   height: 184,
-//         //   callback: function () {
-//         //     return {
-//         //       customField: {
-//         //         title: 'My Custom Field',
-//         //         value: t.card('custom', 'customField')
-//         //       }
-//         //     };
-//         //   }
-//         // });
-//       }
-//     }
-//   ];
-//   }
-// });
-
-// TrelloPowerUp.initialize({
-//   'card-buttons': function (t, options) {
-//     return [
-//       {
-//         icon: './icon.svg',
-//         text: 'Custom Field',
-//         callback: function (t) {
-//           return t.popup({
-//             title: 'My Custom Field',
-//             url: './custom-field.html',
-//             height: 184,
-//             callback: function () {
-//               return {
-//                 customField: {
-//                   title: 'My Custom Field',
-//                   value: t.card('custom', 'customField')
-//                 }
-//               };
-//             }
-//           });
-//         }
-//       }
-//     ];
-//   }
-// });
+              return t.set('card', 'shared', 'tag', tag)
+                .then(function() {
+                  return t.closePopup();
+                });
+            }
+          });
+        }
+      }];
+    },
+    'card-detail-badges': function(t, options) {
+      return t.get('card', 'shared', 'tag')
+        .then(function(tag) {
+          if (!tag) {
+            return [];
+          }
+          return [{
+            text: tag.text,
+            color: tag.color
+          }];
+        });
+    }
+  });
+  
+  function getTagColor(value) {
+    switch (value) {
+      case 'bug':
+        return 'red';
+      case 'task':
+        return 'yellow';
+      case 'story':
+        return 'blue';
+      case 'epic':
+        return 'green';
+      default:
+        return 'gray';
+    }
+  }
+  
