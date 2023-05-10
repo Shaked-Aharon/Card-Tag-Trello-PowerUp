026fcb605bbf74c98ac6a091f65fb7f6ce4e7671
Shaked Aharon <shakedaha@matrix.co.il>
2023-05-10 23:45:16 +0300

    Added custom field

--- index.js
+++ index.js
@@ -8,44 +8,68 @@ var onBtnClick = function (t, opts) {
 
 TrelloPowerUp.initialize({
   'card-buttons': function (t, opts) {
-    return [{
+    return [
+      {
       // usually you will provide a callback function to be run on button click
       // we recommend that you use a popup on click generally
       icon: GRAY_ICON, // don't use a colored icon here
       text: 'Open Popup',
       callback: onBtnClick,
       condition: 'edit'
-    }, {
+    },
+    {
       // but of course, you could also just kick off to a url if that's your thing
       icon: GRAY_ICON,
       text: 'Just a URL',
       condition: 'always',
       url: 'https://developer.atlassian.com/cloud/trello',
       target: 'Trello Developer Site' // optional target for above url
-    }];
+    },
+    {
+      icon: './icon.svg',
+      text: 'Custom Field',
+      callback: function (t) {
+        return t.popup({
+          title: 'My Custom Field',
+          url: './custom-field.html',
+          height: 184,
+          callback: function () {
+            return {
+              customField: {
+                title: 'My Custom Field',
+                value: t.card('custom', 'customField')
+              }
+            };
+          }
+        });
+      }
+    }
+  ];
   }
 });
 
-// TrelloPowerUp.initialize({
-//   'card-buttons': function (t, options) {
-//     return [{
-//       icon: './icon.svg',
-//       text: 'Custom Field',
-//       callback: function (t) {
-//         return t.popup({
-//           title: 'My Custom Field',
-//           url: './custom-field.html',
-//           height: 184,
-//           callback: function () {
-//             return {
-//               customField: {
-//                 title: 'My Custom Field',
-//                 value: t.card('custom', 'customField')
-//               }
-//             };
-//           }
-//         });
-//       }
-//     }];
-//   }
-// });
+TrelloPowerUp.initialize({
+  'card-buttons': function (t, options) {
+    return [
+      {
+        icon: './icon.svg',
+        text: 'Custom Field',
+        callback: function (t) {
+          return t.popup({
+            title: 'My Custom Field',
+            url: './custom-field.html',
+            height: 184,
+            callback: function () {
+              return {
+                customField: {
+                  title: 'My Custom Field',
+                  value: t.card('custom', 'customField')
+                }
+              };
+            }
+          });
+        }
+      }
+    ];
+  }
+});
