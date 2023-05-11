000a31e55afcb3d9a06bce8a7a76a4840bbd83bd
Shaked Aharon <shakedaha@matrix.co.il>
2023-05-11 08:16:16 +0300

    Try Promise all

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
 window.TrelloPowerUp.initialize({
   "card-badges": function (t, opts) {
-    return t.get('card', 'shared', 'tag')
-      .then(selectedTag => [
-        tag.Badge(selectedTag, badageTypes.badge),
-        priority.Badge(null, badageTypes.badge)
-      ]);
+    // return t.get('card', 'shared', 'tag')
+    //   .then(selectedTag => [
+    //     tag.Badge(selectedTag, badageTypes.badge),
+    //     priority.Badge(null, badageTypes.badge)
+    //   ]);
+
+      return Promise.all([t.get('card', 'shared', 'tag'), t.get('card', 'shared', 'priority')])
+      .then(([tag, priority]) => {
+        return [
+          tag.Badge(tag, badageTypes.badge),
+          priority.Badge(priority, badageTypes.badge)
+        ]
+
+      })
   },
   'card-buttons': function (t, options) {
     return [tag.Btn(), priority.Btn()];
   },
   'card-detail-badges': function (t, options) {
-    return t.get('card', 'shared', 'tag')
-      .then(selectedTag => [
-        tag.Badge(selectedTag, badageTypes.detailsBadge)
-      ]);
+    // return t.get('card', 'shared', 'tag')
+    //   .then(selectedTag => );
+    return Promise.all([t.get('card', 'shared', 'tag'), t.get('card', 'shared', 'priority')])
+      .then(([tag, priority]) => {
+        return [
+          tag.Badge(tag, badageTypes.detailsBadge),
+          priority.Badge(priority, badageTypes.detailsBadge)
+        ]
+
+      })
   }
 });
 
