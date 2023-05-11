d6149a76343cb6086d620ce9401f837a970be0f2
Shaked Aharon <shakedaha@matrix.co.il>
2023-05-11 19:01:49 +0300

    Added full path

--- public/js/icons.js
+++ public/js/icons.js
@@ -1,9 +1,9 @@
 const icons = {
-    tag: '/public/icons/tag.svg',
-    priority_highest: '/public/icons/priority-highest.svg',
-    priority_high: '/public/icons/priority-high.svg',
-    priority_medium: '/public/icons/priority-medium.svg',
-    priority_low: '/public/icons/priority-low.svg',
-    priority_lowest: '/public/icons/priority-lowest.svg',
-    priority: '/public/icons/priority.svg',
+    tag: 'https://shaked-aharon.github.io/Card-Tag-Trello-PowerUp/public/icons/tag.svg',
+    priority_highest: 'https://shaked-aharon.github.io/Card-Tag-Trello-PowerUp/public/icons/priority-highest.svg',
+    priority_high: 'https://shaked-aharon.github.io/Card-Tag-Trello-PowerUp/public/icons/priority-high.svg',
+    priority_medium: 'https://shaked-aharon.github.io/Card-Tag-Trello-PowerUp/public/icons/priority-medium.svg',
+    priority_low: 'https://shaked-aharon.github.io/Card-Tag-Trello-PowerUp/public/icons/priority-low.svg',
+    priority_lowest: 'https://shaked-aharon.github.io/Card-Tag-Trello-PowerUp/public/icons/priority-lowest.svg',
+    priority: 'https://shaked-aharon.github.io/Card-Tag-Trello-PowerUp/public/icons/priority.svg',
 }
