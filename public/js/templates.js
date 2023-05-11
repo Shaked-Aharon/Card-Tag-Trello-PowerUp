7523efc46e809938b3bca99c163359b4ae34ebe9
Shaked Aharon <shakedaha@matrix.co.il>
2023-05-12 02:40:11 +0300

    Try URL object

--- public/js/templates.js
+++ public/js/templates.js
@@ -10,10 +10,14 @@ const BugTemplate = `####**Step to Reproduce:**####
 function setBugTemplate(t) {
     Promise.all([t.getRestApi().getToken(), t.card('id')])
         .then(([token, card]) => {
-            if (!token) {console.log('Not Authoraized, Cant Set Template'); return;}
-            console.log({ token, id:card.id, BugTemplate });
-            const url = `https://api.trello.com/1/cards/${card.id}?key=f3066f5108e24c693700a5ac80e00dec&token=${token}&desc=${BugTemplate}`;
-            fetch(encodeURI(url), { method: 'PUT' })
+            if (!token) { console.log('Not Authoraized, Cant Set Template'); return; }
+            console.log({ token, id: card.id, BugTemplate });
+            let url = new URL(`https://api.trello.com/1/cards/${card.id}`);
+            url.searchParams.append('key', 'f3066f5108e24c693700a5ac80e00dec');
+            url.searchParams.append('token', token);
+            url.searchParams.append('desc', BugTemplate);
+            // const url = `https://api.trello.com/1/cards/${card.id}?key=f3066f5108e24c693700a5ac80e00dec&token=${token}&desc=${BugTemplate}`;
+            fetch(url, { method: 'PUT' })
                 .then(res => {
                     console.log(res);
                     return res.json();
