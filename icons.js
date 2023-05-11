2ce63fed005417306e96c7d7007817a8b7c24f7f
Shaked Aharon <shakedaha@matrix.co.il>
2023-05-11 09:24:59 +0300

    New Icons

--- icons.js
+++ icons.js
@@ -1,5 +1,9 @@
 const icons = {
     tag: './tag.svg',
-    priority_highest: './Highest-priority.svg',
+    priority_highest: './highest-priority.svg',
+    priority_high: './high-priority.svg',
+    priority_medium: './medium-priority.svg',
+    priority_low: './low-priority.svg',
+    priority_lowest: './lowest-priority.svg',
     priority: './priority.svg',
 }
