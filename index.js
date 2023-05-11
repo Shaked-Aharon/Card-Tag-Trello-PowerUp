90ca6882c8848929d629d6fc542ad133df7577b0
Shaked Aharon <shakedaha@matrix.co.il>
2023-05-11 17:31:18 +0300

    Different approach

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 window.TrelloPowerUp.initialize({
   "card-badges": function (t, opts) {
-    t.card('all').then(card => console.log({card}))
-    t.set('card', 'desc', 'lorespm epsum lorespm epsumlorespm epsumlorespm epsumlorespm epsumlorespm epsumlorespm epsumlorespm epsumlorespm epsumlorespm epsumlorespm epsumlorespm epsumlorespm epsumlorespm epsumlorespm epsumlorespm epsumlorespm epsumlorespm epsumlorespm epsumlorespm epsumlorespm epsumlorespm epsumlorespm epsumlorespm epsumlorespm epsumlorespm epsumlorespm epsumlorespm epsumlorespm epsumlorespm epsumlorespm epsumlorespm epsumlorespm epsumlorespm epsumlorespm epsum')
-    .then(() => {
-      console.log('changed!');
+    t.card('all').then(({ card }) => {
+      console.log({ card })
+
+      t.set('card', 'desc', 'lorespm epsum lorespm epsumlorespm epsumlorespm epsumlorespm epsumlorespm epsumlorespm epsumlorespm epsumlorespm epsumlorespm', { id: card.id })
+        .then(() => {
+          console.log('changed!');
+        })
     })
-    t.get('card', 'shared').then(shared => console.log({shared}))
-    t.get('card', 'private').then(private => console.log({private}))
-      return Promise.all([t.get('card', 'shared', 'tag'), t.get('card', 'shared', 'priority')])
+
+    t.get('card', 'shared').then(shared => console.log({ shared }))
+    t.get('card', 'private').then(private => console.log({ private }))
+    return Promise.all([t.get('card', 'shared', 'tag'), t.get('card', 'shared', 'priority')])
       .then(([selectedTag, selectedPriority]) => {
         return [
           tag.Badge(selectedTag, badageTypes.badge),
@@ -21,7 +25,7 @@ window.TrelloPowerUp.initialize({
   },
   'card-detail-badges': function (t, options) {
     return Promise.all([t.get('card', 'shared', 'tag'), t.get('card', 'shared', 'priority')])
-    .then(([selectedTag, selectedPriority]) => {
+      .then(([selectedTag, selectedPriority]) => {
         return [
           tag.Badge(selectedTag, badageTypes.detailsBadge),
           priority.Badge(selectedPriority, badageTypes.detailsBadge)
