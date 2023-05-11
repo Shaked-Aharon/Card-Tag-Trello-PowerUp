fcb1f9d57426ad2cde3e550749c0399d354a9fd4
Shaked Aharon <shakedaha@matrix.co.il>
2023-05-11 10:13:44 +0300

    return to old code

--- priority.js
+++ priority.js
@@ -20,18 +20,18 @@ const priority = {
     },
     Badge(priority, badageType = badageTypes.badge) {
         if (!priority) return;
+        const badge = {
+            text: priority.text,
+            icon: priority.icon,
+            showOnClose: true,
+            title: 'Priority'
+        }
         if (this.type === badageTypes.badge) {
-            return {
-                icon: priority.icon,
-            };
+            badge.showOnClose = undefined;
+            badge.text = undefined;
+            badge.title = undefined;
         }
-        if (this.type === badageTypes.detailsBadge) {
-            return {
-                text: priority.text,
-                showOnClose: true,
-                title: 'Priority'
-            };
-        }
+        return badge;
     }
 }
 
