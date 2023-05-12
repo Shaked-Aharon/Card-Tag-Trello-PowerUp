ef140779fda3b18a761ae60b9fb19591b06ebcde
Shaked Aharon <shakedaha@matrix.co.il>
2023-05-12 03:03:15 +0300

    Added template for task

--- public/js/templates.js
+++ public/js/templates.js
@@ -10,7 +10,44 @@ const templates = {
     
     [tagType.story]: `"As a [persona]": Who are we building this for? We’re not just after a job title, we’re after the persona of the person. Max. Our team should have a shared understanding of who Max is. We’ve hopefully interviewed plenty of Max’s. We understand how that person works, how they think and what they feel. We have empathy for Max.
     “Wants to”: Here we’re describing their intent — not the features they use. What is it they’re actually trying to achieve? This statement should be implementation free — if you’re describing any part of the UI and not what the user goal is you're missing the point.
-    “So that”: how does their immediate desire to do something this fit into their bigger picture? What’s the overall benefit they’re trying to achieve? What is the big problem that needs solving?`
+    “So that”: how does their immediate desire to do something this fit into their bigger picture? What’s the overall benefit they’re trying to achieve? What is the big problem that needs solving?`,
+    [tagType.task]: `
+    ### :green book: Summary
+    **As a** 
+    **I want to** 
+    **So that** 
+     
+    ### :clipboard: Acceptance Criteria
+    /info panel
+    #### Acceptance Criteria 1
+    **Given** precondition
+    **When** action
+    **Then** result
+    
+    /info panel
+    #### Acceptance Criteria 2
+    **Given** precondition
+    **When** action
+    **Then** result
+     
+    ### :paintbrush: Design
+    
+    #### Link to Screendesign
+    [Links to your design system like InVision, Sketch,etc.](http://linkhere.com)
+    
+    #### Wireframes
+    Place Wireframes, Sketches, Scribbles, etc. here
+     
+    ### :floppy_disk: Technical Information
+    #### Link to technical information
+    Any link to technical documents, e.g. Swagger Documentation, Architectural diagram can be added here
+    
+    ### :notebook: Additional Notes / Information
+    Any notes you took during refinement sessions or talks with other people about the story
+     
+    ### :flag on: ToDo
+    (-) @ clarify`
+
 }
 
 const StoryTemplate = 
