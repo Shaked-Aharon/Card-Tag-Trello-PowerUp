47c4c036316376a21a4bad6e0937fd36cc33bdea
Shaked Aharon <shakedaha@matrix.co.il>
2023-05-12 03:31:32 +0300

    Update templates

--- public/js/templates.js
+++ public/js/templates.js
@@ -1,73 +1,89 @@
 const templates = {
     [tagType.bug]: `
-    ### Environment ###
-    * Device model:
-    * OS version:
-    * Software version:
-    * Browser version
-    * Stage:
-    * Account:
-     
-    ### Precondition ###
-    *
-     
-    ### Steps to reproduce ###
-    #
-     
-    ### Current behavior ###
-    *
-     
-    ### Expected behavior ###
-    *
-    ### Relevant Assets ### (like id or barcode or product or client)`,
-    
-    [tagType.task]: `
-    ### What needs to be done ###
+### Environment
 
-    ### Why it needs to be done ###
-    
-    ### Acceptance Criteria ###
-    #
-    #
-    
-    ### Additional Information ###
+- Device model:
+- OS version:
+- Software version:
+- Browser version
+- Stage:
+- Account:
+
+### Precondition
+
+\*
+
+### Steps to reproduce
+
+#
+
+### Current behavior
+
+\*
+
+### Expected behavior
+
+\*
+    `,
+
+    [tagType.task]: `
+### What needs to be done
+
+### Why it needs to be done
+
+### Acceptance Criteria
+
+### Additional Information
     `,
     [tagType.story]: `
-    ### :green book: Summary ###
-    **As a** 
-    **I want to** 
-    **So that** 
-     
-    ### :clipboard: Acceptance Criteria ###
-    /info panel
-    #### Acceptance Criteria 1 ####
-    **Given** precondition
-    **When** action
-    **Then** result
-    
-    /info panel
-    #### Acceptance Criteria 2 ####
-    **Given** precondition
-    **When** action
-    **Then** result
-     
-    ### :paintbrush: Design ###
-    
-    #### Link to Screendesign ####
-    [Links to your design system like InVision, Sketch,etc.](http://linkhere.com)
-    
-    #### Wireframes ####
-    Place Wireframes, Sketches, Scribbles, etc. here
-     
-    ### :floppy_disk: Technical Information ###
-    #### Link to technical information ####
-    Any link to technical documents, e.g. Swagger Documentation, Architectural diagram can be added here
-    
-    ### :notebook: Additional Notes / Information ###
-    Any notes you took during refinement sessions or talks with other people about the story
-     
-    ### :flag on: ToDo ###
-    (-) @ clarify`
+### :green book: Summary
+
+**As a**
+**I want to**
+**So that**
+
+### :clipboard: Acceptance Criteria
+
+/info panel
+
+#### Acceptance Criteria 1
+
+**Given** precondition
+**When** action
+**Then** result
+
+/info panel
+
+#### Acceptance Criteria 2
+
+**Given** precondition
+**When** action
+**Then** result
+
+### :paintbrush: Design
+
+#### Link to Screendesign
+
+[Links to your design system like InVision, Sketch,etc.](http://linkhere.com "‌")
+
+#### Wireframes
+
+Place Wireframes, Sketches, Scribbles, etc. here
+
+### :floppy_disk: Technical Information
+
+#### Link to technical information
+
+Any link to technical documents, e.g. Swagger Documentation, Architectural diagram can be added here
+
+### :notebook: Additional Notes / Information
+
+Any notes you took during refinement sessions or talks with other people about the story
+
+### :flag on: ToDo
+
+(-) \@ clarify
+`
 
 }
 
