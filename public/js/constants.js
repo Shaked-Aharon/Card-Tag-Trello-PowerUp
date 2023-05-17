36be361c93f336e0fb2cb3795a4f6fd095b39f1d
Shaked Aharon <shakedaha@matrix.co.il>
2023-05-17 12:26:36 +0300

    update baseurl

--- public/js/constants.js
+++ public/js/constants.js
@@ -1,4 +1,4 @@
-// const BASEURL = "https://shaked-aharon.github.io/Card-Tag-Trello-PowerUp";
-const BASEURL = "https://f73b-2a0d-6fc2-63d0-4400-9cff-b42f-f06f-86e0.ngrok-free.app";
+const BASEURL = "https://shaked-aharon.github.io/Card-Tag-Trello-PowerUp";
+// const BASEURL = "https://f73b-2a0d-6fc2-63d0-4400-9cff-b42f-f06f-86e0.ngrok-free.app";
 const KEY = "f3066f5108e24c693700a5ac80e00dec"
 const POWERUP_NAME = "Agile Utils";
