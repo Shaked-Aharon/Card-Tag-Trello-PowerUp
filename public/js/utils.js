4377f76e9486126c421e2bcc8561ec992bb551a3
Shaked Aharon <srek123@gmail.com>
2024-08-17 14:57:44 +0300

    Update the log

--- public/js/utils.js
+++ public/js/utils.js
@@ -11,7 +11,7 @@ function getColorBasedOnLabels(labels) {
 
 function applyBackgroundColor(cardId, color) {
     var cardElement = document.querySelector(`div[data-testid="trello-card"][data-card-id="${cardId}"]`);
-    console.log(`Try finding element to paint: ${cardId}`)
+    console.log(`Try finding element to paint: 'div[data-testid="trello-card"][data-card-id="${cardId}"]'`, cardElement)
     if (cardElement) {
         console.log(`Try painting card element usig this color: ${color}`);
         cardElement.style.backgroundColor = color;
