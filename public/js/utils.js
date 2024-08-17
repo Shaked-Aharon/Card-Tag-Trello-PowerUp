785092cc1442b111ed032cb5e04691ff83820afc
Shaked Aharon <srek123@gmail.com>
2024-08-17 14:37:34 +0300

    Added 'account' scope and try to use token with api request to retrive lastActivity of card

--- public/js/utils.js
+++ public/js/utils.js
@@ -1,28 +1,33 @@
 function getColorBasedOnLabels(labels) {
     if (labels.some(label => label.color === 'green')) {
-      return '#D4EDDA'; // Light green for "Done"
+        return '#D4EDDA'; // Light green for "Done"
     } else if (labels.some(label => label.color === 'yellow')) {
-      return '#FFF3CD'; // Light yellow for "In Progress"
+        return '#FFF3CD'; // Light yellow for "In Progress"
     } else if (labels.some(label => label.color === 'red')) {
-      return '#F8D7DA'; // Light red for "Urgent"
+        return '#F8D7DA'; // Light red for "Urgent"
     }
     return '#FFFFFF'; // Default white
-  }
-  
-  function applyBackgroundColor(cardId, color) {
+}
+
+function applyBackgroundColor(cardId, color) {
     var cardElement = document.querySelector(`.list-card[data-card-id="${cardId}"]`);
     if (cardElement) {
-      cardElement.style.backgroundColor = color;
+        cardElement.style.backgroundColor = color;
     }
-  }
+}
 
-  function fetchCardLastActivity(t, cardId) {
+function fetchCardLastActivity(t, cardId) {
     return t.getRestApi()
-      .get(`/cards/${cardId}/actions`, { filter: 'updateCard:moveCardToBoard,moveCardFromBoard,updateCard:closed' })
-      .then(function(actions) {
-        if (actions && actions.length > 0) {
-          return actions[0].date; // Return the date of the most recent action
-        }
-        return null;
-      });
-  }
+        .getToken()
+        .then(token => {
+            if (!token) throw 'Invalid or missing token.'
+            return t.getRestApi()
+                .get(`/cards/${cardId}/actions`, { filter: 'updateCard:moveCardToBoard,moveCardFromBoard,updateCard:closed' })
+                .then(function (actions) {
+                    if (actions && actions.length > 0) {
+                        return actions[0].date; // Return the date of the most recent action
+                    }
+                    return null;
+                });
+        })
+}
