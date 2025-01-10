57354dd995b635b9b7c6c662e06625a9c69c3e8d
Shaked Aharon <srek123@gmail.com>
2025-01-10 16:54:22 +0200

    Set to work with shalev api key

--- public/js/card-badges.controller.js
+++ public/js/card-badges.controller.js
@@ -1,50 +1,50 @@
 const cardBadagesController = (t, opts) => {
   return Promise.all([t.card('all'), t.get('card', 'shared', 'tag'), t.get('card', 'shared', 'priority'), t.get('board', 'shared', 'card-number-prefix')])
     .then(([card, selectedTag, selectedPriority, cardNumberPrefix]) => {
-      // return fetchCardLastActivity(t, card.id).then(lastActivity => {
+      return fetchCardLastActivity(t, card.id).then(lastActivity => {
 
         const badges = [
           { text: `${cardNumberPrefix}-${card.idShort}`, showOnClose: true },
           tag.Badge(selectedTag, badageTypes.badge),
           priority.Badge(selectedPriority, badageTypes.badge)
         ]
-        // if (lastActivity) {
-        //   var activityDate = new Date(lastActivity);
-        //   var now = new Date();
-        //   var timeDifference = now - activityDate; // Time difference in milliseconds
-        //   var daysDifference = timeDifference / (1000 * 3600 * 24); // Convert to days
+        if (lastActivity) {
+          var activityDate = new Date(lastActivity);
+          var now = new Date();
+          var timeDifference = now - activityDate; // Time difference in milliseconds
+          var daysDifference = timeDifference / (1000 * 3600 * 24); // Convert to days
 
-        //   var coverColor;
-        //   if (daysDifference <= 1) {
-        //     coverColor = 'green'; // Green for recently updated (within 1 day)
-        //   } else if (daysDifference <= 7) {
-        //     coverColor = 'yellow'; // Yellow for updates within the last week
-        //   } else {
-        //     coverColor = 'red'; // Red for older updates
-        //   }
+          var coverColor;
+          if (daysDifference <= 1) {
+            coverColor = 'green'; // Green for recently updated (within 1 day)
+          } else if (daysDifference <= 7) {
+            coverColor = 'yellow'; // Yellow for updates within the last week
+          } else {
+            coverColor = 'red'; // Red for older updates
+          }
 
-        //   // Use set to change the cover
-        //   t.set('card', 'shared', 'cover', {
-        //     color: coverColor,
-        //     brightness: 'light'
-        //   });
-        //   badges.push({
-        //     text: 'עודכן לאחרונה: ' + daysDifference.toFixed(0) + ' ימים',
-        //     color: coverColor
-        //   });
-        // }
-        // if (card.due) {
-        //   var dueDate = new Date(card.due);
-        //   var now = new Date();
-        //   if (dueDate < now) {
-        //     badges.push({
-        //       text: 'Overdue!',
-        //       color: 'red'
-        //     });
-        //   }
-        // }
+          // Use set to change the cover
+          t.set('card', 'shared', 'cover', {
+            color: coverColor,
+            brightness: 'light'
+          });
+          badges.push({
+            text: 'עודכן לאחרונה: ' + daysDifference.toFixed(0) + ' ימים',
+            color: coverColor
+          });
+        }
+        if (card.due) {
+          var dueDate = new Date(card.due);
+          var now = new Date();
+          if (dueDate < now) {
+            badges.push({
+              text: 'Overdue!',
+              color: 'red'
+            });
+          }
+        }
         return badges
-      // })
+      })
     })
 
 }
